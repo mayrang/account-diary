ff7593364657410c596229b1884953f0790d87b8
mayrang <pkss0626@naver.com>
2022-11-20 05:12:29 +0900

    server 설정

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,4 +3,31 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { AppDataSource } from "./data-source";
 
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}));
+app.use(cookieParser());
+
+let port = 4000;
+
+
+app.listen(port, async () => {
+    console.log(`Server running ${port} Port`);
+    AppDataSource.initialize()
+    .then(() => {
+        console.log("database init!");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
