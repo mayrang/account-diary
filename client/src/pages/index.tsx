e6ae10d13bffa77020844e670d0e5ecf3e4f53e3
mayrang <pkss0626@naver.com>
2022-11-20 04:02:21 +0900

    home 디자인 변경

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,54 +1,17 @@
+import Link from "next/link";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <>
-    <Header />
-   <div className="text-semibold underline h-16">
-    hello world!
+    <div className="h-screen w-full bg-emerald-300 flex justify-center items-center">
+        <div className="flex flex-col items-center justify-center">
+            <div className="text-2xl font-bold">Mayrang</div>
+            <div className="mt-14 flex items-center">
+                <Link href={"/register"}><a className="p-2 bg-white rounded border">회원가입</a></Link>
+                <Link href={"/login"}><a className="py-2 px-4 ml-8 bg-white rounded border">로그인</a></Link>
+            </div>
+        </div>
+   
    </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   <div className="text-semibold underline h-16">
-    hello world!
-   </div>
-   </>
   )
 }
